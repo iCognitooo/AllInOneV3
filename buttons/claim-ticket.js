d7a12ae159b36d0ef6be6ba162d4a6f6943b5171
iCognitoo <moralesherrerajuanpablo62@gmail.com>
2025-05-15 02:41:42 +0000

    Cambios

--- buttons/claim-ticket.js
+++
@@ -1,8 +0,0 @@
-const { claimTicket } = require("../utils/ticket-manager")
-
-module.exports = {
-  customId: "claim-ticket",
-  async execute(interaction) {
-    await claimTicket(interaction)
-  },
-}
